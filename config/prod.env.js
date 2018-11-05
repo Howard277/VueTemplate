90a4213c11b08fe35a43c8f963b537b0e3a24898
Howard277 <wuketao@houbank.com>
2018-11-05 09:30:47 +0800

    补充生产环境配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  
+  // 统一登录配置
+  host: '"localhost"',
+  port: '"8000"',
+  uamRootUrl: '"http://localhost:8081/"',
+  targetAppCode: '"testApp"'
 }
